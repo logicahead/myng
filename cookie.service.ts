da35c853669ad48fd4f3523563cba6b21fece695
root <root@vinay-Inspiron-3542.(none)>
2017-02-28 23:37:24 +0530

    d

--- cookie.service.ts
+++ cookie.service.ts
@@ -35,6 +35,17 @@ export class CookieService {
     this.setCookie(cname, '', -1);
   }
 
+ function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
   function getCookie(name) {
   var value = "; " + document.cookie;
   var parts = value.split("; " + name + "=");
