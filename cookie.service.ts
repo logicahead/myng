8def19e56ca729d47817d489a6e1a1ec2890abc1
root <root@vinay-Inspiron-3542.(none)>
2017-03-01 06:21:04 +0530

    d

--- cookie.service.ts
+++ cookie.service.ts
@@ -57,6 +57,22 @@ export class CookieService {
     return this.getAll()[key]
 }
 
+getCookie(cname) {
+        var name = cname + "=";
+        var ca = document.cookie.split(';');
+        for(var i = 0; i <ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0)==' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                if(c.substring(name.length,c.length) == "") return false;
+                return c.substring(name.length,c.length);
+            }
+        }
+        return "";
+}
+
   function getCookie(name) {
   var value = "; " + document.cookie;
   var parts = value.split("; " + name + "=");
